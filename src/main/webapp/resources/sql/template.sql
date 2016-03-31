644a481d6a8082c928a385bd51e698f9516c595e
Christian <Christian@Christian-PC>
2016-03-31 00:17:17 -0400

    Mas mecanismo de empresas

--- src/main/webapp/resources/sql/template.sql
+++ src/main/webapp/resources/sql/template.sql
@@ -1,4 +1,4 @@
-
+﻿
 
 INSERT INTO usuario (id, estado, fecharegitro, fechaultimamodificacion, nombre, password, username) VALUES (1, 'ACTIVO', NULL, NULL, 'Christian Romero', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'admin');
 
